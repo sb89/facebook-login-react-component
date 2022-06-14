ea797841ee75235a359d1dafc454265367740cd5
steven <s.blake.191289@gmail.com>
2022-06-14 22:13:32 +0100

    Fixed types

--- index.d.ts
+++ index.d.ts
@@ -8,64 +8,70 @@ export interface RenderProps {
     onClick(): void
 }
 
-export interface FacebookLoginProps {
-    appId: string;
-    callback(userInfo: FacebookLoginInfo | FacebookFailureResponse): void;
-    onFailure?(response: FacebookFailureResponse): void;
-    render?(renderProps: RenderProps): React.ReactNode;
-    autoLoad?: boolean;
-    buttonStyle?: React.CSSProperties;
-    containerStyle?: React.CSSProperties;
-    cookie?: boolean;
-    cssClass?: string;
-    disableMobileRedirect?: boolean;
-    fields?: string;
-    icon?: React.ReactNode;
-    isDisabled?: boolean;
-    language?: string;
-    onClick?(event: React.MouseEvent<HTMLDivElement>): void;
-    reAuthenticate?: boolean;
-    redirectUri?: string;
-    scope?: string;
-    size?: "small" | "medium" | "metro";
-    textButton?: string;
-    typeButton?: string;
-    version?: string;
-    xfbml?: boolean;
-    isMobile?: boolean;
-    tag?: Node | React.Component<any>;
-    returnScopes?: boolean;
-    state?: string;
-    authType?: string;
-    responseType?: string;
+export interface FacebookFailureResponse {
+  status?: string;
 }
 
-export interface FacebookFailureResponse {
-    status?: string;
-}
-  
 export interface FacebookLoginInfo {
-    first_name: string;
-    last_name: string;
-    email?: string;
-    picture?: {
-      data: {
-        height?: number;
-        is_silhouette?: boolean;
-        url?: string;
-        width?: number;
-      }
-    },
-    birthday: string;
-    id: string;
-    "accessToken": string;
-    "userID": string;
-    "expiresIn": number;
-    "signedRequest": string;
-    "graphDomain": string;
-    "data_access_expiration_time": number;
+  first_name: string;
+  last_name: string;
+  email?: string;
+  picture?: {
+    data: {
+      height?: number;
+      is_silhouette?: boolean;
+      url?: string;
+      width?: number;
+    }
+  },
+  birthday: string;
+  id: string;
+  "accessToken": string;
+  "userID": string;
+  "expiresIn": number;
+  "signedRequest": string;
+  "graphDomain": string;
+  "data_access_expiration_time": number;
+}
+
+interface FacebookLoginSharedProps {
+  appId: string;
+  callback(userInfo: FacebookLoginInfo | FacebookFailureResponse): void;
+  onFailure?(response: FacebookFailureResponse): void;
+  autoLoad?: boolean;
+  cookie?: boolean;
+  disableMobileRedirect?: boolean;
+  fields?: string;
+  isDisabled?: boolean;
+  language?: string;
+  onClick?(event: React.MouseEvent<HTMLDivElement>): void;
+  reAuthenticate?: boolean;
+  redirectUri?: string;
+  scope?: string;
+  version?: string;
+  xfbml?: boolean;
+  isMobile?: boolean;
+  returnScopes?: boolean;
+  state?: string;
+  authType?: string;
+  responseType?: string;
+};
+
+export interface FacebookLoginProps extends FacebookLoginSharedProps {
+    render(renderProps: RenderProps): React.ReactNode;
+}
+
+export interface FacebookLoginWithButtonProps extends FacebookLoginSharedProps {
+  buttonStyle?: React.CSSProperties;
+  containerStyle?: React.CSSProperties;
+  cssClass?: string;
+  icon?: React.ReactNode;
+  size?: "small" | "medium" | "metro";
+  textButton?: string;
+  typeButton?: string;
+  tag?: Node | React.Component<any>;
 }
 
 export const FacebookLogin: React.FunctionComponent<FacebookLoginProps>
 
-export const FacebookLoginWithButton: React.FunctionComponent<FacebookLoginProps>
+export const FacebookLoginWithButton: React.FunctionComponent<FacebookLoginWithButtonProps>
